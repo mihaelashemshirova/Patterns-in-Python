44f8689d5912591cd82b77ec5cafad735c1d381d
Mihaela Shemshirova <121877004+mihaelashemshirova@users.noreply.github.com>
2023-02-22 22:56:48 +0200

    Create Rhombus of Stars.py

---
+++ Rhombus of Stars.py
@@ -0,0 +1,21 @@
+def rhombus_of_stars(n):
+    for i in range(1, n + 1):
+        spaces = n - i
+        stars = i
+        print(' ' * spaces + '* ' * stars)
+
+    for i in range(n - 1, 0, -1):
+        spaces = n - i
+        stars = i
+        print(' ' * spaces + '* ' * stars)
+
+
+size = int(input('Enter size of rhombus: '))
+rhombus_of_stars(size)
+
+# Enter size of rhombus: 3
+#  * 
+# * * 
+#* * * 
+# * * 
+#  * 
